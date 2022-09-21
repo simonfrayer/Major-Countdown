309cbd428e454ff504a1ed9eac9e0e4a9d0e9629
Simon Mayer <315183@via.dk>
2022-09-21 22:33:29 +0200

    update of the number by the difference of days

--- logic.js
+++ logic.js
@@ -1,12 +1,24 @@
 let numberElement = document.getElementById("number");
-let cislo = 32;
+let cislo = 1;
+
+const today = new Date();
+const dd = String(today.getDate()).padStart(2, '0');
+const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+const yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+
+const startDate = new Date('9/13/2022');
+
+const diffTime = Math.abs(today - startDate);
+const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
 
 numberElement.innerHTML = cislo;
 
-let intervalUpdate = setInterval(updateCislo, 86400000);
-
-
 function updateCislo(){
-    cislo *= 2;
-    numberElement.innerHTML = cislo;
+    cislo = Math.pow(cislo, getDayDifference);
 };
+
+function getDayDifference(){
+    return diffDays;
+}
